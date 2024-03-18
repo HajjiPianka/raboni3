65a9159ed201f919769419a36fae35bc9d5338fa
Pawelo112 <elopawelo112@gmail.com>
2024-03-18 12:55:33 +0100

    Caly projekt

---
+++ src/main/java/pl/edu/pbs/projekt_spring_boot_lab3/ProjektSpringBootLab3Application.java
@@ -0,0 +1,13 @@
+package pl.edu.pbs.projekt_spring_boot_lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektSpringBootLab3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektSpringBootLab3Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pbs/projekt_spring_boot_lab3/ProjektSpringBootLab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pbs.projekt_spring_boot_lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektSpringBootLab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
